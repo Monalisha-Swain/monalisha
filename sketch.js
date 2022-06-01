74cc685d93075fffffa9a5e8362638efcb284fd3
unknown <monalishaswain38@gmail.com>
2022-06-01 18:09:27 +0530

    calculator

---
+++ sketch.js
@@ -0,0 +1,29 @@
+var h2 = document.querySelector(".calender-piture h2")
+var h3 = document.querySelector(".calender-piture h3")
+ 
+var monthArr = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "Auguest",
+    "September",
+    "october",
+    "November",
+    "December",
+];
+
+var dayArr =[
+    "Sunday",
+    "Monday",
+    "Tuseday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "saturday",
+];
+
+
